0c5180448d7bbd868c5c44244af38764af661029
Steven Oliver Smith <smit3114@purdue.edu>
2020-01-18 00:24:55 -0500

    Added the "quote" command. Updated the "dum" command to pull randomly from a file of quotes. Changed the console messages to include timestamps.

--- Squire/Squire/Squire.py
+++ Squire/Squire/Squire.py
@@ -1,20 +1,26 @@
 #Squire.py
 import os
 import random
+import time
 
 from discord.ext import commands
 from dotenv import load_dotenv
 
+def log(MESSAGE):
+	LOCAL_TIME = time.localtime(time.time())
+	TIMESTAMP = f'{LOCAL_TIME.tm_mday}-{LOCAL_TIME.tm_mon}-{LOCAL_TIME.tm_year} {LOCAL_TIME.tm_hour}:{LOCAL_TIME.tm_min}:{LOCAL_TIME.tm_sec}'
+	print(f'\n{TIMESTAMP} {MESSAGE}')
 
-print(f'Loading enviroment variables...')
+log('Loading enviroment variables...')
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-print(f'\nCreating bot instance...')
+log('Creating bot instance...')
 BOT = commands.Bot(command_prefix='!')
 
 @BOT.command(name='r', help='Rolls dice! (Format: [number_of_dice]d[number_of_sides])')
 async def roll_dice(CTX, *DICE):
+	log('Running the dice roll command...')
 	REQUEST_USR = CTX.author.mention
 	if not DICE:
 		RESPONSE = f'{REQUEST_USR} needs to learn how to give the correct parameters. (No parameter not given)'
@@ -48,17 +54,42 @@ async def roll_dice(CTX, *DICE):
 					RESPONSE = f'{REQUEST_USR} rolled **{DICE_TOTAL}** on **{DICE[0]}**. {DICE_RESULT}'
 				else:
 					RESPONSE = f'{REQUEST_USR} rolled **{DICE_RESULT[0]}** on a **{DICE[0]}**.'
+	log('Finished running the dice roller command!')
+	await CTX.send(RESPONSE)
 
+@BOT.command(name='quote', help='Either adds or reads off a random quote. MAKE SURE TO ADD THE QUOTATION MARKS IF YOU\'RE ADDING A QUOTE! (Format: "[quote-text]"')
+async def quote(CTX, *QUOTE):
+	log('Now running the quote command...')
+
+	if not QUOTE:
+		with open(f'{os.getcwd()}/Quotes.txt', 'r') as f:
+			LINES = f.read().splitlines()
+			SELECTED_INDEX = random.choice(range(0, len(LINES)))
+			RESPONSE = LINES[SELECTED_INDEX]
+	else:
+		with open(f'{os.getcwd()}/Quotes.txt', 'a') as f:
+			f.write(f'\n{QUOTE[0]}')
+		RESPONSE = f'Added the quote "{QUOTE[0]}" to the list!' 
+
+	log('Finished running the quote command!')
 	await CTX.send(RESPONSE)
 
 @BOT.command(name='dum', help='U iz dum.')
 async def dum(CTX):
+	log('Now running the dum command...')
 	REQUEST_USR = CTX.author.mention
-	await CTX.send(f'{REQUEST_USR} is the big dum.')
+	
+	with open(f'{os.getcwd()}/DumQuotes.txt', 'r') as f:
+		LINES = f.read().splitlines()
+		SELECTED_INDEX = random.choice(range(0, len(LINES)))
+		DUM_LINE = LINES[SELECTED_INDEX]
+
+	log('Finished running the dum command!')
+	await CTX.send(f'{REQUEST_USR} {DUM_LINE}')
 
 @BOT.event
 async def on_ready():
-	print(f'\n{BOT.user.name} has connected to Discord!')
+	log(f'{BOT.user.name} has connected to Discord!')
 
-print(f'\nSending bot to server...')
+log('Sending bot to server...')
 BOT.run(TOKEN)
