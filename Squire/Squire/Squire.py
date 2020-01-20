0844917c2bf0c8e790ba4fc75a255441c80acda5
Steven Oliver Smith <smit3114@purdue.edu>
2020-01-20 18:36:49 -0500

    Added the ability for Dum quotes to be gifs or pngs

--- Squire/Squire/Squire.py
+++ Squire/Squire/Squire.py
@@ -2,6 +2,7 @@
 import os
 import random
 import time
+import discord
 
 from discord.ext import commands
 from dotenv import load_dotenv
@@ -11,24 +12,25 @@ def log(MESSAGE):
 	TIMESTAMP = f'{LOCAL_TIME.tm_year}-{LOCAL_TIME.tm_mon}-{LOCAL_TIME.tm_mday} {LOCAL_TIME.tm_hour}:{LOCAL_TIME.tm_min}:{LOCAL_TIME.tm_sec}'
 	print(f'{TIMESTAMP} {MESSAGE}')
 
-log('Loading enviroment variables...')
+log('Loading enviroment variables')
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+ANNOUNCEMENT_CHANNEL = os.getenv('ANNOUCMENT_CHANNEL')
 
-log('Creating bot instance...')
+log('Creating bot instance')
 BOT = commands.Bot(command_prefix='!')
 
 @BOT.command(name='r', help='Rolls dice! (Format: [number_of_dice]d[number_of_sides])')
 async def roll_dice(CTX, *DICE):
-	log('Running the dice roll command...')
-	REQUEST_USR = CTX.author.mention
+	REQUEST_USR = CTX.author
+	log(f'Running the dice roll command for {REQUEST_USR}')
 	if not DICE:
-		RESPONSE = f'{REQUEST_USR} needs to learn how to give the correct parameters. (No parameter not given)'
+		RESPONSE = f'{REQUEST_USR.mention} needs to learn how to give the correct parameters. (No parameter not given)'
 	else:
 		SEPERATOR_INDEX = DICE[0].lower().find('d')
 
 		if SEPERATOR_INDEX == -1:
-			RESPONSE = f'{REQUEST_USR} needs to learn how to give the correct parameters. ("**{DICE[0]}**" is not valid parameter)'
+			RESPONSE = f'{REQUEST_USR.mention} needs to learn how to give the correct parameters. ("**{DICE[0]}**" is not valid parameter)'
 		else:
 			SPLIT = DICE[0].lower().split('d')
 
@@ -37,7 +39,7 @@ async def roll_dice(CTX, *DICE):
 				DICE_NUM = int(SPLIT[0])
 				DICE_SIDES = int(SPLIT[1])
 			except ValueError:
-				RESPONSE = f'{REQUEST_USR} needs to learn how to give the correct parameters. ("**{DICE[0]}**" is not valid parameter)'
+				RESPONSE = f'{REQUEST_USR.mention} needs to learn how to give the correct parameters. ("**{DICE[0]}**" is not valid parameter)'
 				TEST = False
 
 			if TEST:
@@ -51,15 +53,15 @@ async def roll_dice(CTX, *DICE):
 					for NUM in DICE_RESULT:
 						DICE_TOTAL = DICE_TOTAL + int(NUM)
 					
-					RESPONSE = f'{REQUEST_USR} rolled **{DICE_TOTAL}** on **{DICE[0]}**. {DICE_RESULT}'
+					RESPONSE = f'{REQUEST_USR.mention} rolled **{DICE_TOTAL}** on **{DICE[0]}**. {DICE_RESULT}'
 				else:
-					RESPONSE = f'{REQUEST_USR} rolled **{DICE_RESULT[0]}** on a **{DICE[0]}**.'
-	log('Finished running the dice roller command!')
+					RESPONSE = f'{REQUEST_USR.mention} rolled **{DICE_RESULT[0]}** on a **{DICE[0]}**.'
 	await CTX.send(RESPONSE)
 
 @BOT.command(name='quote', help='Either adds or reads off a random quote. MAKE SURE TO ADD THE QUOTATION MARKS IF YOU\'RE ADDING A QUOTE! (Format: "[quote-text]"')
 async def quote(CTX, *QUOTE):
-	log('Now running the quote command...')
+	REQUEST_USR = CTX.author
+	log(f'Now running the quote command for {REQUEST_USR}')
 
 	if not QUOTE:
 		with open(f'{os.getcwd()}/Quotes.txt', 'r') as f:
@@ -71,25 +73,26 @@ async def quote(CTX, *QUOTE):
 			f.write(f'\n{QUOTE[0]}')
 		RESPONSE = f'Added the quote "{QUOTE[0]}" to the list!' 
 
-	log('Finished running the quote command!')
 	await CTX.send(RESPONSE)
 
 @BOT.command(name='dum', help='U iz dum.')
 async def dum(CTX):
-	log('Now running the dum command...')
-	REQUEST_USR = CTX.author.mention
+	REQUEST_USR = CTX.author
+	log(f'Now running the dum command for {REQUEST_USR}')
 	
 	with open(f'{os.getcwd()}/DumQuotes.txt', 'r') as f:
 		LINES = f.read().splitlines()
 		SELECTED_INDEX = random.choice(range(0, len(LINES)))
 		DUM_LINE = LINES[SELECTED_INDEX]
 
-	log('Finished running the dum command!')
-	await CTX.send(f'{REQUEST_USR} {DUM_LINE}')
+	if DUM_LINE.endswith('.gif') or DUM_LINE.endswith('.png'):
+		await CTX.send(f'{REQUEST_USR.mention}', file=discord.File(f'Assets\{DUM_LINE}'))
+	else:
+		await CTX.send(f'{REQUEST_USR.mention} {DUM_LINE}')
 
 @BOT.event
 async def on_ready():
 	log(f'{BOT.user.name} has connected to Discord!')
 
-log('Sending bot to server...')
+log('Sending bot to server')
 BOT.run(TOKEN)
